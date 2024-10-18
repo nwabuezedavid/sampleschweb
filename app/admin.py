9df589733a86c21c283f8c8fc81c5a37d6973c26
NwabuezeDavid <davidnwabueze333@gmail.com>
2024-10-18 21:33:55 +0100

    Your commit message

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import * 
 
+
+
+admin.site.register(siteedit)
 # Register your models here.
--- app/views.py
+++ app/views.py
@@ -3,66 +3,66 @@ from .models import *
 # Create your views here.
 def home(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "home.html",con)
 def discover(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "discove.html",con)
 def teology(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "teology.html",con)
 def study(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "study.html",con)
 def Engage(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "Engage.html",con)
 def Connect(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "Connect.html",con)
 def Library(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "Library.html",con)
 def WorkingatTrinity(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "WorkingatTrinity.html",con)
 def student(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "student.html",con)
 def WaystoGive(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "WaystoGive.html",con)
 def recutment(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "recutment.html",con)
 def EYFS(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "EYFS.html",con)
 def infortechcom(request):
     con ={
-        # 'site':siteedit.objects.get(idx=1)
+        'site':siteedit.objects.get(idx=1)
     }
     return render (request, "infortechcom.html",con)
